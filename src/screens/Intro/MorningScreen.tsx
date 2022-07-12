9fce072ea1145bfac740d3e9c21942329d9acc20
HyejinUm <uhj1993@naver.com>
2022-07-12 18:29:17 +0900

    Feat: 횡방향 스크롤 만들기

--- src/screens/Intro/MorningScreen.tsx
+++ src/screens/Intro/MorningScreen.tsx
@@ -1,12 +1,21 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, ScrollView, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import AppLayout from '~/components/AppLayout';
 import Theme from '~/styles/theme';
 import Button from '~/components/Button';
 import {useNavigation} from '@react-navigation/native';
-
+import {TextMedium, TextRegular} from '~/components/Text';
+import {TabRoutineJson} from '~/utils/routine';
+import type {TabRoutineType} from '~/utils/routine';
 import type {StackNavigationProp} from '@react-navigation/stack';
 import type {RootStackParamList} from 'types/navigation';
+import {FC} from 'react';
 
 type PrevNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -19,17 +28,31 @@ const MorningScreen = () => {
   const [morningTodo, setMorningTodo] = useState([]);
 
   useEffect(() => {}, [morningTodo]);
+
   const handleAfternoonPage = () =>
     morningTodo.length > 0 && navigation.navigate('AfternoonScreen');
+
+  const HorizontalTabSection: FC<TabRoutineType> = ({title}) => {
+    return (
+      <TouchableOpacity
+        style={styles.horizontalTabContent}
+        activeOpacity={0.6}
+        onPress={() => {
+          console.log('test');
+        }}>
+        <Text style={styles.horizontalTabContentText}>{title}</Text>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <AppLayout>
       <View style={styles.container}>
         <View>
-          <Text>아침을 어떻게 보내고 싶나요?</Text>
+          <TextMedium>아침을 어떻게 보내고 싶나요?</TextMedium>
         </View>
         <View>
-          <Text>
+          <TextRegular>
             {morningTodo.length === 0
               ? `${username}님의 아침을 활력있게 만들어 줄 루틴을 골라보세요`
               : morningTodo.length === 1
@@ -37,8 +60,13 @@ const MorningScreen = () => {
               : morningTodo.length === 2
               ? '원하는 아침 루틴을 직접 만들 수도 있어요'
               : '모두 골랐다면 오후 루틴으로 넘어가요 :)'}
-          </Text>
+          </TextRegular>
         </View>
+        <ScrollView horizontal={true}>
+          {TabRoutineJson.map(item => {
+            return <HorizontalTabSection key={item.key} title={item.title} />;
+          })}
+        </ScrollView>
         <Button
           isCorrect={morningTodo.length > 0}
           isLoading={false}
@@ -61,5 +89,16 @@ const styles = StyleSheet.create({
     paddingRight: Theme.width(16),
     paddingLeft: Theme.width(16),
     marginTop: Theme.height(40),
+  },
+
+  horizontalTabContent: {
+    paddingHorizontal: Theme.width(16),
+    paddingVertical: Theme.height(8),
+    borderRadius: Theme.width(5),
+    backgroundColor: Theme.colors.main,
+    marginRight: Theme.width(8),
+  },
+  horizontalTabContentText: {
+    color: Theme.colors.white,
   },
 });
---
+++ src/utils/routine.ts
@@ -0,0 +1,27 @@
+export type TabRoutineType = {
+  key: number;
+  title: string;
+};
+
+export const TabRoutineJson: TabRoutineType[] = [
+  {
+    key: 0,
+    title: '건강',
+  },
+  {
+    key: 1,
+    title: '셀프케어',
+  },
+  {
+    key: 2,
+    title: '성장',
+  },
+  {
+    key: 3,
+    title: '생산성',
+  },
+  {
+    key: 4,
+    title: '기타',
+  },
+];
