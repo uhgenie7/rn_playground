452ed78b086b2038aced6631f36df810ad3fc1e9
HyejinUm <uhj1993@naver.com>
2022-07-12 10:08:37 +0900

    Feat: #12 navigation 연동

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -3,6 +3,9 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../types/navigation';
 import LogIn from '~/screens/login';
 import MorningScreen from '~/screens/Intro/MorningScreen';
+import AfternoonScreen from '~/screens/Intro/AfternoonScreen';
+import DinnerScreen from '~/screens/Intro/DinnerScreen';
+import CheckScreen from '~/screens/Intro/CheckScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -15,6 +18,9 @@ const RootStack = () => {
         }}>
         <Stack.Screen name="Login" component={LogIn} />
         <Stack.Screen name="MorningScreen" component={MorningScreen} />
+        <Stack.Screen name="AfternoonScreen" component={AfternoonScreen} />
+        <Stack.Screen name="DinnerScreen" component={DinnerScreen} />
+        <Stack.Screen name="CheckScreen" component={CheckScreen} />
       </Stack.Navigator>
     </>
   );
---
+++ src/screens/Intro/DinnerScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import AppLayout from '~/components/AppLayout';
+
+const DinnerScreen = () => {
+  return (
+    <AppLayout>
+      <View>
+        <Text>Test</Text>
+      </View>
+    </AppLayout>
+  );
+};
+
+export default DinnerScreen;
